1d21921c9125f78bce729bd3db96ed5e5e18fdae
Chris Bradfield <chris@kidscancode.org>
2017-02-24 12:31:38 -0800

    commit 101 Part 5

---
+++ Godot101/Part 05/player.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+export var speed = 400
+
+var screensize
+var extents
+var vel = Vector2()
+
+func _ready():
+	set_fixed_process(true)
+	screensize = get_viewport_rect().size
+	extents = get_node("sprite").get_texture().get_size() / 4
+	set_pos(screensize / 2)
+	print(extents)
+
+func _fixed_process(delta):
+	var input = Vector2(0, 0)
+	input.x = Input.is_action_pressed("ui_right") - Input.is_action_pressed("ui_left")
+	input.y = Input.is_action_pressed("ui_down") - Input.is_action_pressed("ui_up")
+	vel = input.normalized() * speed
+	var pos = get_pos() + vel * delta
+	pos.x = clamp(pos.x, extents.width, screensize.width - extents.width)
+	pos.y = clamp(pos.y, extents.height, screensize.height - extents.height)
+	set_pos(pos)
+
