be1138399e878979a984dcaac82ac1acde7b85ed
Chris Bradfield <chris@kidscancode.org>
2017-04-06 09:18:32 -0700

    edits

--- Godot101/working/player.gd
+++ Godot101/working/player.gd
@@ -1,10 +1,29 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+const ACCEL = 1500
+const MAX_SPEED = 500
+const FRICTION = -500
+const GRAVITY = 2000
+
+var acc = Vector2()
+var vel = Vector2()
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	print(delta)
+	acc.y = GRAVITY
+	acc.x = Input.is_action_pressed("ui_right") - Input.is_action_pressed("ui_left")
+	acc.x *= ACCEL
+	if acc.x == 0:
+		acc.x = vel.x * FRICTION * delta
+	vel += acc * delta
+	vel.x = clamp(vel.x, -MAX_SPEED, MAX_SPEED)
+
+	var motion = move(vel * delta)
+	if is_colliding():
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		vel = n.slide(vel)
+		move(motion)
