eb86d1aaf3d9a27783637dbb23a0af5478c4e7d2
Chris Bradfield <chris@kidscancode.org>
2017-04-30 14:37:47 -0700

    Space Rocks tutorial - Part 08

---
+++ Space_Rocks_tutorial/Part 08/scripts/player_bullet.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var vel = Vector2()
+export var speed = 1000
+
+func _ready():
+	set_fixed_process(true)
+
+func start_at(dir, pos, v):
+	set_rot(dir)
+	set_pos(pos)
+	vel = Vector2(speed, 0).rotated(dir + PI/2) # + v
+
+func _fixed_process(delta):
+	set_pos(get_pos() + vel * delta)
+
+func _on_lifetime_timeout():
+	queue_free()
+
+func _on_player_bullet_body_enter( body ):
+	if body.get_groups().has("asteroids"):
+		queue_free()
+		body.explode(vel.normalized())
